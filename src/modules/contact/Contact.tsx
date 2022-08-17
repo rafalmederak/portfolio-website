5d1a1c1ecc4b4bd84d2b21d00ec1f65b65717441
rafalmederak <rafal.mederak1@gmail.com>
2022-08-17 09:52:18 +0200

    Added submit function to the Contact form

--- src/modules/contact/Contact.tsx
+++ src/modules/contact/Contact.tsx
@@ -1,8 +1,44 @@
 import Header from "@components/header/Header";
+import React, { useState } from "react";
 import * as Styled from "./Contact.styles";
 import { contactList } from "./ContactList";
 
 const Contact = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const changeValues = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) =>
+    setFormValues((values) => ({ ...values, [e.target.name]: e.target.value }));
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/sendgrid", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formValues),
+      });
+      if (!res.ok) {
+        setFormValues({
+          name: "",
+          email: "",
+          message: "",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Styled.Container>
       <Styled.SectionWrapper>
@@ -12,10 +48,27 @@ const Contact = () => {
           you want.
         </p>
         <Styled.FormContainer>
-          <Styled.FormWrapper method="post">
-            <input type="text" placeholder="Name" name="name" required />
-            <input type="email" placeholder="Email" name="email" required />
-            <textarea placeholder="Message" name="message" required />
+          <Styled.FormWrapper method="post" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              required
+              onChange={changeValues}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              required
+              onChange={changeValues}
+            />
+            <textarea
+              placeholder="Message"
+              name="message"
+              required
+              onChange={changeValues}
+            />
             <button type="submit">Send a message</button>
           </Styled.FormWrapper>
           <Styled.ContactMethodsWrapper>
