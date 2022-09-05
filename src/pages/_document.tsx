c169b5c730dcb24c2f46c6052210965c420445ad
rafalmederak <rafal.mederak1@gmail.com>
2022-09-05 14:50:02 +0200

    Added Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,6 +39,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="/logo.svg" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
