627c9d155bccd824fa001e49c2dbc94eab03dcd1
rafalmederak <rafal.mederak1@gmail.com>
2022-09-08 15:27:29 +0200

    Added Meta Tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,20 +7,29 @@ import Contact from "@modules/contact/Contact";
 
 import { hygraphClient } from "src/common/hygraph/client";
 import { postsQuery } from "src/common/hygraph/queries";
-
+import Head from "next/head";
 import { IPosts } from "@modules/projects/Projects.interfaces";
 
 const Home = ({ posts }: IPosts) => {
   return (
-    <Layout>
-      <>
-        <Hero />
-        <About />
-        <Skills />
-        <Projects posts={posts} />
-        <Contact />
-      </>
-    </Layout>
+    <>
+      <Head>
+        <title>Rafał Męderak | Web development</title>
+        <meta
+          name="description"
+          content="I am a website freelancer from Poland, and I study at the Higher School of Economy and IT in Cracow. I have been creating websites as a freelance web developer for the last three years."
+        />
+      </Head>
+      <Layout>
+        <>
+          <Hero />
+          <About />
+          <Skills />
+          <Projects posts={posts} />
+          <Contact />
+        </>
+      </Layout>
+    </>
   );
 };
 
