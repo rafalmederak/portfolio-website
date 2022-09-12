76c1855f81ab07f00d42bd82ec6a4ed71100dff8
rafalmederak <rafal.mederak1@gmail.com>
2022-09-12 16:52:51 +0200

    Configured the MUI server-side rendering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,27 @@ import type { AppProps } from "next/app";
 import GlobalStyle from "@styles/globalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@styles/theme";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "src/common/utils/createEmotionCache";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache();
+
+interface IAppProps extends AppProps {
+  emotionCache: EmotionCache;
+}
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: IAppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle theme={theme} />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle theme={theme} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,21 +8,38 @@ import Document, {
 
 import { ServerStyleSheet } from "styled-components";
 
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "../common/utils/createEmotionCache";
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App: any) => (props) =>
+            sheet.collectStyles(<App emotionCache={cache} {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
+
+      const emotionStyles = extractCriticalToChunks(initialProps.html);
+      const emotionStyleTags = emotionStyles.styles.map((style) => (
+        <style
+          data-emotion={`${style.key} ${style.ids.join(" ")}`}
+          key={style.key}
+          dangerouslySetInnerHTML={{ __html: style.css }}
+        />
+      ));
+
       return {
         ...initialProps,
+        emotionStyleTags,
         styles: (
           <>
             {initialProps.styles}
@@ -52,6 +69,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@300;400;600;700&display=swap"
             rel="stylesheet"
           />
+          {(this.props as any).emotionStyleTags}
         </Head>
         <body>
           <Main />
